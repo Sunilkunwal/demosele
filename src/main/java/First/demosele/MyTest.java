aefc26d2a979dc1b5fbc34e5e4dc95a8f3f5b6cb
Sunilkunwal <sunilkunwal97@gmail.com>
2024-03-13 19:05:30 +0530

    vhv

--- src/main/java/First/demosele/MyTest.java
+++ src/main/java/First/demosele/MyTest.java
@@ -2,29 +2,44 @@ package First.demosele;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MyTest {
     public static void main(String[] args) {
-        // Specify the path to your ChromeDriver executable
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\pc\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+    	
+    	//Invoking Browser 
+    	//Chrome - ChromeDriver exten-->Methods close get
+    	//Firefox - FirefoxDriver -->methods close get
+    	//safari SafariDriver -->methods close get
+    	//WebDriver close get
+    	//WebDrive methods + Chrome browser 
+    	
+    	
+    	//chromedriver.exe--> chrome browser
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\pc\\Downloads\\Selenium Drivers\\chromedriver.exe");
+        
 
-        // Instantiate ChromeOptions
-        ChromeOptions options = new ChromeOptions();
-
-        // Add headless argument
-        options.addArguments("--headless");
-
-        // Pass ChromeOptions to ChromeDriver
-        WebDriver driver = new ChromeDriver(options);
-
-        // Example: Navigate to a website
-        driver.get("https://astro.synilogictech.com/");
-
-        // Example: Print the page title
-        System.out.println("Page title is: " + driver.getTitle());
-
-        // Close the browser
-        driver.quit();
+        // webdriver.chrome.driver-->value of path
+        WebDriver driver = new ChromeDriver();
+        
+        //firefox Launch
+        //geckodriver
+        
+        //webdriver.gecko.driver
+//    	System.setProperty("webdriver.gecko.driver", "C:\\Users\\pc\\Downloads\\Selenium Drivers\\geckodriver.exe");
+//        WebDriver driver1 = new FirefoxDriver();
+        
+        //webdriver.gecko.driver
+//    	System.setProperty("webdriver.edge.driver", "C:\\Users\\pc\\Downloads\\Selenium Drivers\\msedgedriver.exe");
+//        WebDriver driver2 = new EdgeDriver();
+        
+        
+        driver.get("https://astro.synilogictech.com");
+       
+       System.out.println(driver.getTitle());
+       System.out.println(driver.getCurrentUrl());
+       driver.close();
+//       driver.quit();
     }
 }
