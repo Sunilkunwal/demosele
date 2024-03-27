efe531311464c111a3904e2a47a302336dfdbd16
Sunilkunwal <sunilkunwal97@gmail.com>
2024-03-27 19:05:32 +0530

    ss

--- src/main/java/First/demosele/AstroSyni.java
+++ src/main/java/First/demosele/AstroSyni.java
@@ -1,5 +1,8 @@
 package First.demosele;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -17,8 +20,8 @@ public class AstroSyni {
     	
     	
     	//chromedriver.exe--> chrome browser
-        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\pc\\Downloads\\Selenium Drivers\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", "/home/sunil/Downloads/Selenium Drivers/chromedriver-linux64/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\pc\\Downloads\\Selenium Drivers\\chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "/home/sunil/Downloads/Selenium Drivers/chromedriver-linux64/chromedriver");
         
 
         // webdriver.chrome.driver-->value of path
@@ -38,11 +41,28 @@ public class AstroSyni {
 //        WebDriver driver2 = new EdgeDriver();
         
         
-        driver.get("https://astro.synilogictech.com");
+        driver.get("https://astrovastutalks.com/vendor-register");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
        
-       System.out.println(driver.getTitle());
-       System.out.println(driver.getCurrentUrl());
-       driver.close();
+//       System.out.println(driver.getTitle());
+//       System.out.println(driver.getCurrentUrl());
+//       driver.close();
 //       driver.quit();
+        driver.findElement(By.id("validationCustom01")).sendKeys("Sunil");
+        driver.findElement(By.id("inputEmail4")).sendKeys("sunial@gmail.com");
+        driver.findElement(By.id("inputPassword4")).sendKeys("9874564111");
+        driver.findElement(By.name("firm_name")).sendKeys("Ram lal and son's Traders");
+        driver.findElement(By.id("gst_no")).sendKeys("AAA09QWERT1234HAD");
+        driver.findElement(By.id("autocomplete")).sendKeys("kota");
+        driver.findElement(By.id("inputZip")).sendKeys("324009");
+        driver.findElement(By.className("note-editable")).sendKeys("test");
+        driver.findElement(By.className("astro-btn")).click();
+        
+        
+        driver.close();
+        
+      
+        
+        
     }
 }
