49853ffc0f73b91d11e854c90313e7e2d7fc6bef
sunil <sunilkunwal97@gmail.com>
2024-04-06 01:14:15 +0530

    autosaggestive dropdown

--- src/main/java/First/demosele/Kundli.java
+++ src/main/java/First/demosele/Kundli.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -43,6 +44,9 @@ public class Kundli {
 
 	      //place of birh 
 	     
+	      
+	    
+
 
 	     
 	        
