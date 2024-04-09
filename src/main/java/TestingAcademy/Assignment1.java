13e68fca55855be1b00917b7386658b8ed957f2e
sunil <sunilkunwal97@gmail.com>
2024-04-10 02:31:37 +0530

    Assignment

---
+++ src/main/java/TestingAcademy/Assignment1.java
@@ -0,0 +1,33 @@
+package TestingAcademy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "/home/sunil/Downloads/Selenium Drivers/chromedriver-linux64/chromedriver");
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://rahulshettyacademy.com/angularpractice/");
+		 driver.findElement(By.name("name")).sendKeys("sunil");
+		 driver.findElement(By.name("email")).sendKeys("sunilkunwal@gmail.com");
+		 driver.findElement(By.id("exampleInputPassword1")).sendKeys("123456");
+		 driver.findElement(By.id("exampleCheck1")).click();
+		 
+		 WebElement staticDropdown = driver.findElement(By.id("exampleFormControlSelect1"));
+		 Select dropdown = new Select(staticDropdown);
+		 dropdown.selectByVisibleText("Female");
+		 
+		 driver.findElement(By.id("inlineRadio2")).click();
+		 driver.findElement(By.xpath("//input[@name='bday']")).sendKeys("04/10/2024");
+		 driver.findElement(By.xpath("//input[@value='Submit']")).click();
+		 System.out.println(driver.findElement(By.cssSelector(".alert.alert-success.alert-dismissible")).getText());
+
+	}
+
+}
